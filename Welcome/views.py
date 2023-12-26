b6b5f91ac9ec888b1437b77cd9b8473c3314943a
Alireza <alirezagoldoos@gmail.com>
2023-12-27 00:36:29 +0330

    adding searchbar to welcome.html

--- Welcome/views.py
+++ Welcome/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from Specialty.models import speciality
 
 # Create your views here.
 def welcome(request):
-    return render(request, 'Welcome/wellcome.html')
+    specialities = speciality.objects.all()
+    print(specialities)
+    return render(request, 'Welcome/wellcome.html', context={'specialities' : specialities})
