b533dea24665da21fa04424812b86a1fb32d1f97
Alireza <alirezagoldoos@gmail.com>
2023-12-27 12:17:45 +0330

    Fixing homepage html

--- Welcome/views.py
+++ Welcome/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from Specialty.models import speciality
+from django.http.response import HttpResponse, JsonResponse
+from Specialty.models import speciality, doctor
 
 # Create your views here.
 def welcome(request):
+    if request.method == 'POST':
+        selected_specialty = request.POST['selected_specialty']
+        doctors = doctor.objects.filter(Specialty__Name=selected_specialty)
+        return render(request, 'Logy/logy.html', context={'doctors':doctors})
     specialities = speciality.objects.all()
-    print(specialities)
     return render(request, 'Welcome/wellcome.html', context={'specialities' : specialities})
+                                                                
+# def goto_specialty(request):
+    # return HttpResponse(selected_specialty)
